e82c5b638aeeb6775c92ef64663598fcae8beee1
TomChiu <iiooiio@hotmail.com>
2020-02-24 00:54:24 +0800

    debug

--- src/main/java/com/it/dao/impl/AccountDaoImpl.java
+++ src/main/java/com/it/dao/impl/AccountDaoImpl.java
@@ -4,7 +4,7 @@ import com.it.dao.IAccountDao;
 
 public class AccountDaoImpl implements IAccountDao {
 
-    @Override
+
     public void saveAccount() {
         System.out.println("account created!");
     }
